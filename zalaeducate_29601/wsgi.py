a687c8a5899634ca88b5f238450fad148d32e570
Crowdbotics <team@crowdbotics.com>
2021-08-07 13:06:36 +0000

    Update models

--- zalaeducate_29601/wsgi.py
+++ zalaeducate_29601/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zalaeducate_29601.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zalaeducate_29601.settings")
 
 application = get_wsgi_application()
